18c36370f980b0883e855005b0e15c30fd2657e5
AK507 <dev.ankitkumar507@gmail.com>
2026-05-26 11:55:59 +0530

    Product Page changes - AddToCart

--- assets/product-info.js
+++ assets/product-info.js
@@ -355,6 +355,7 @@ if (!customElements.get('product-info')) {
         this.setQuantityBoundries();
 
         const quantityFormUpdated = html.getElementById(`Quantity-Form-${sectionId}`);
+        if (!quantityFormUpdated) return;
         const selectors = ['.quantity__input', '.quantity__rules', '.quantity__label'];
         for (let selector of selectors) {
           const current = this.quantityForm.querySelector(selector);
